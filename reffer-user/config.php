94795e05803c32d81c77d45717f67720b5ed8033
Will0376 <15551918+Will0376@users.noreply.github.com>
2019-03-07 22:33:27 +0300

    Update config.php

--- reffer-user/config.php
+++ reffer-user/config.php
@@ -2,12 +2,21 @@
 if (! defined ( 'DATALIFEENGINE' )) {
 	die ( "ПОШЕЛ НАХУЙ!" );
 } //защита файла
-$connect_error = 'Не удалось подсоеденится к базе данных!';
-
 $mysql_host = 'localhost';//хост БД
 $mysql_user = 'root';//юзер БД
-$mysql_pass = 'root';//пароль БД
-$mysql_db = 'database';//база с БД
+$mysql_pass = 'pass';//пароль БД
+$mysql_db = 'db';//база с БД
+
+$mysql_playtime_host = 'localhost';
+$mysql_playtime_user = 'root';
+$mysql_playtime_pass = 'pass';
+$mysql_playtime_db = 'playtime';
+
+$connect_error = 'Не удалось подсоеденится к базе данных!';
+$connect_error_select = 'Ошибка выбора бд!';
+$connect_error_playtime = 'Ошибка подключения к бд со временем !';
+$connect_error_select_playtime = 'Ошибка выбора бд времени!';
+
 
 
 if (!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_db)) {
